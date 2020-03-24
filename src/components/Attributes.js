11d4c042210de82926a0c01b34f5ae523f949f62
KelechiOdom10 <kelechiodom10@gmail.com>
2020-03-24 16:07:45 +0000

    Added a new Attributes file in the component folder for the function

---
+++ src/components/Attributes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Attributes(props) {
+    return(
+      <div>
+          <p><b>{props.title}</b>{props.value}</p>
+      </div>
+
+    )
+}
+
+export default Attributes
